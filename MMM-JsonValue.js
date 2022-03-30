4051c4e6e1f1e4514affffb01f22ace5bb213635
Matt Bryce <mattbryce@icloud.com>
2022-03-30 23:16:54 +0100

    Added numberFormatLocale option, updated ReadMe and added addtional screenshot

--- MMM-JsonValue.js
+++ MMM-JsonValue.js
@@ -17,13 +17,9 @@ Module.register("MMM-JsonValue", {
 		jsonPath: "content",
 		headers: {},
 		iconTally: false, // If set to true, the value will be processed as a tally of the configured icon.
-		NumberFormat: null, // "undefined, { style: 'currency', currency: 'GBP' }",
-					// "en-GB, { style: 'unit', unit: 'miles-per-hour' }"
-		//financial: false, // If set to true, value will be processed via Intl.NumberFormat object according to set style, currency and minorUnit  
-		//style: 'currency', // Intl.NumberFormat style option
-		//currency: 'GBP', // aIntl.NumberFormat currency option
-		minorUnits: false, // Used by NumberFormat to return minor units. Set to true if value returned is in pence for example, 250 will be set 2.50 before passing into Intl.NumberFormat
-								
+		numberFormat: null, // "undefined, { style: 'currency', currency: 'GBP' }", or "en-GB, { style: 'unit', unit: 'miles-per-hour' }"			
+		numberFormatLocale: "undefined", // "undefined" Uses the the implementation's default locale, change this to a BCP 47 language tag if required
+		minorUnits: false, // Used by NumberFormat to return minor units. Set to true if value returned is in pence for example, 250 will be set 2.50 before passing into Intl.NumberFormat						
 		refreshInterval: 1000 * 60, // refresh every minute
 	},
 
@@ -50,46 +46,46 @@ Module.register("MMM-JsonValue", {
 			return wrapper;
 		}
         //Process with Intl.NumberFormat if set in config
-        if (this.config.NumberFormat) {
+        if (this.config.numberFormat) {
 			//Treat the value as a minorUnit e.g pence. "250" will be converted to "2.50" or "25000" will be "250.00"
-if (this.config.minorUnits){
-	var value = this.value / 100;
-}
-else {
-	var value = this.value;
-}
+			if (this.config.minorUnits){
+				var value = this.value / 100;
+			}
+			else {
+				var value = this.value;
+			}
 
-// Create the number formatter. Will use the default
-var formatter = new Intl.NumberFormat(undefined, this.config.NumberFormat );
+		// Create the number formatter. Will use the default
+		var formatter = new Intl.NumberFormat(this.config.numberFormatLocale, this.config.numberFormat );
 
-var formattedValue = formatter.format(value); // e.g.£2,500.00 or 250000 miles
+		var formattedValue = formatter.format(value); // e.g.£2,500.00 or 250000 miles
 
-wrapper.innerHTML = this.config.prefix + formattedValue + this.config.suffix;
+		wrapper.innerHTML = this.config.prefix + formattedValue + this.config.suffix;
 
-if(this.config.icon) {
-	wrapper.innerHTML = "<span class=\"" + this.config.icon + "\"></span>" + wrapper.innerHTML;
-} 
-}
-
-//Create the icon tally
-else if(this.config.iconTally){
-	  var intValue = parseInt(this.value); //convert value from string to integer for the for loop
-	  wrapper.innerHTML = wrapper.innerHTML + this.config.prefix;
-	for (let i = 0; i < intValue; i++) {
-	  wrapper.innerHTML = wrapper.innerHTML + "<span class=\"" + this.config.icon + "\"></span>";
-  }
-	wrapper.innerHTML = wrapper.innerHTML + this.config.suffix;
-}
-else { //otherwise process value as normal
-	wrapper.innerHTML = this.config.prefix + this.value + this.config.suffix;
-	if(this.config.icon) {
-		wrapper.innerHTML = "<span class=\"" + this.config.icon + "\"></span>" + wrapper.innerHTML;
-	} 
-}
-		if(this.config.skipPadding) {
-			wrapper.style = "margin-block-end: -30px;";
+			if(this.config.icon) {
+				wrapper.innerHTML = "<span class=\"" + this.config.icon + "\"></span>" + wrapper.innerHTML;
+			} 
 		}
-		return wrapper;
+
+		//Create the icon tally if set in config
+		else if(this.config.iconTally){
+			var intValue = parseInt(this.value); //convert value from string to integer for the for loop
+			wrapper.innerHTML = wrapper.innerHTML + this.config.prefix;
+				for (let i = 0; i < intValue; i++) {
+					wrapper.innerHTML = wrapper.innerHTML + "<span class=\"" + this.config.icon + "\"></span>";
+				}
+			wrapper.innerHTML = wrapper.innerHTML + this.config.suffix;
+		}
+		else { //otherwise process value as normal
+			wrapper.innerHTML = this.config.prefix + this.value + this.config.suffix;
+				if(this.config.icon) {
+					wrapper.innerHTML = "<span class=\"" + this.config.icon + "\"></span>" + wrapper.innerHTML;
+				} 
+		}
+				if(this.config.skipPadding) {
+					wrapper.style = "margin-block-end: -30px;";
+				}
+			return wrapper;
 	},
 
 	notificationReceived: function() {},
